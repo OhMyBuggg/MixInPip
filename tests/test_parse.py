c061f46efae260d0b0a3350eb32857fd3d18802d
ohmybuggg <sa0963278610@gmail.com>
2020-10-12 21:20:23 +0800

    add ~=, === tests

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -2,52 +2,95 @@ from pip._internal.resolution.resolvelib.provider import PipProvider
 from src.package_source import PackageSource
 from mixology.mixology.range import Range
 from mixology.mixology.union import Union
+from semver.semver.version import Version
 
 def test_parse_equal():
     specifier = "==1.0.0"
     p = PackageSource(PipProvider, None)
-    expected = Range("1.0.0", "1.0.0", True, True)
+    v = Version.parse("1.0.0")
+    expected = Range(v, v, True, True)
 
     assert expected == p.parse_specifier(specifier)
 
 def test_parse_bigger():
     specifier = ">1.0.0"
     p = PackageSource(PipProvider, None)
-    expected = Range("1.0.0", None, False, False)
+    v = Version.parse("1.0.0")
+    expected = Range(v, None, False, False)
 
     assert expected == p.parse_specifier(specifier)
 
 def test_parse_smaller():
     specifier = "<1.0.0"
     p = PackageSource(PipProvider, None)
-    expected = Range(None, "1.0.0", False, False)
+    v = Version.parse("1.0.0")
+    expected = Range(None, v, False, False)
 
     assert expected == p.parse_specifier(specifier)
 
 def test_parse_bigger_include():
     specifier = ">=1.0.0"
     p = PackageSource(PipProvider, None)
-    expected = Range("1.0.0", None, True, False)
+    v = Version.parse("1.0.0")
+    expected = Range(v, None, True, False)
 
     assert expected == p.parse_specifier(specifier)
 
 def test_parse_smaller_include():
     specifier = "<=1.0.0"
     p = PackageSource(PipProvider, None)
-    expected = Range(None, "1.0.0", False, True)
+    v = Version.parse("1.0.0")
+    expected = Range(None, v, False, True)
 
     assert expected == p.parse_specifier(specifier)
 
 def test_parse_not_equal():
     specifier = "!=1.0.0"
     p = PackageSource(PipProvider, None)
-    expected = [Range(None, "1.0.0", False, False), Range("1.0.0", None, False, False)]
+    v = Version.parse("1.0.0")
+    expected = [Range(None, v, False, False), Range(v, None, False, False)]
 
     assert expected == p.parse_specifier(specifier)
 
 def test_parse_padding():
     specifier = "==12.55"
     p = PackageSource(PipProvider, None)
-    expected = Range("12.55", "12.55", True, True)
+    v = Version.parse("12.55.0")
+    expected = Range(v, v, True, True)
 
     assert expected == p.parse_specifier(specifier)
+
+def test_parse_compatible():
+    specifier = "~=2.2"
+    p = PackageSource(PipProvider, None)
+    v = Version.parse("2.2")
+    v2 = Version.parse("3.0")
+    expexted = Range(v, v2, True, False)
+
+    assert expected == p.parse_specifier(specifier)
+
+def test_parse_compatible2():
+    specifier = "~=1.4.5"
+    p = PackageSource(PipProvider, None)
+    v = Version.parse("1.4.5")
+    v2 = Version.parse("1.5")
+    expexted = Range(v, v2, True, False)
+
+    assert expected == p.parse_specifier(specifier)
+
+def test_parse_compatible3():
+    specifier = "~=2.2.0"
+    p = PackageSource(PipProvider, None)
+    v = Version.parse("2.2")
+    v2 = Version.parse("2.3")
+    expexted = Range(v, v2, True, False)
+
+    assert expected == p.parse_specifier(specifier)
+
+def test_parse_arbitrary():
+    # specifier = "===footbar"
+    # p = PackageSource(PipProvider, None)
+    # expexted = Range("footbar", "footbar", True, True)
+
+    # assert expected == p.parse_specifier(specifier)
+    assert True
